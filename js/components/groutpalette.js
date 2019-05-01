542e027f60652b707b0d117bf55738d046c09ab5
Simo Savonen <sipesa@utu.fi>
2019-05-01 16:37:58 +0300

    store uses localStorage, firefox did not like arrow functions as class methods

--- js/components/groutpalette.js
+++ js/components/groutpalette.js
@@ -1,11 +1,24 @@
+import { mapeiGrouts } from './mapeiData.js'
+import { store } from './store.js'
+
 class GroutPalette {
 
   constructor() {
-    console.log('grout palette created')
+    this.groutDiv = document.querySelector('.groutPalette')
+    this.groutHeader = document.querySelector('.groutHeader')
   }
 
   render() {
-    console.log('grout palette rendered')
+    for (let name in mapeiGrouts) {
+      const sampleDiv = document.createElement('div')
+      sampleDiv.className = 'groutSample'
+      sampleDiv.style.background = mapeiGrouts[name]
+      sampleDiv.addEventListener('click', () => {
+        store.setState('groutName', name)
+        store.setState('groutColor', mapeiGrouts[name])
+      })
+      this.groutDiv.appendChild(sampleDiv)
+    }
   }
 
 }
--- js/components/store.js
+++ js/components/store.js
@@ -1,28 +1,65 @@
-class Store {
-  constructor() {
-    console.log('store initialized')
-
-    this.swatch = ['#222e50', '#007991', '#439a86', '#bcd8c1', '#e9d985']
-
-    this.matrixSize = 30 // mosaics sold are up to 26x26
-    this.matrix = []
-    for (let i = 0; i < this.matrixSize; i++) {
-      this.matrix.push([])
-      for (let j = 0; j < this.matrixSize; j++) {
-        this.matrix[i].push(Math.floor(Math.random() * this.swatch.length))
-      }
-    }
-  }
-
-  resetMatrix = (swatchSize) => {
-    for (let i = 0; i < this.matrixSize; i++) {
-      for (let j = 0; j < this.matrixSize; j++) {
-        this.matrix[i][j] = Math.floor(Math.random() * swatchSize)
-      }
-    }
-  }
+const defaults = {
+  'groutName': '100 White',
+  'groutColor': '#FFFFFF',
+  'groutWidth': 4,
+  'tileWidth': 20,
+  'tileHeight': 20,
+  'rows': 10,
+  'cols': 10,
+  'glowEffect': true,
+  'patternLock': false,
 }
 
+class Store {
+  constructor() {
+    this.swatch = JSON.parse(localStorage.getItem('swatch'))
+    this.matrix = JSON.parse(localStorage.getItem('matrix'))
+    this.state = JSON.parse(localStorage.getItem('state'))
 
+    if (!this.swatch) {
+      this.swatch = ['#222e50', '#007991', '#439a86', '#bcd8c1', '#e9d985']
+      localStorage.setItem('swatch', JSON.stringify(this.swatch))
+    }
+
+    if (!this.matrix) {
+      // Populates a 30x30 array with zeros.
+      // ES6 Array.from(object, mapFunction)
+      this.matrix = Array.from({ length: 30 }, () => Array.from({ length: 30 }, () => 0))
+      this.randomizeMatrix()
+    }
+
+    if (!this.state) {
+      this.state = defaults
+      localStorage.setItem('state', JSON.stringify(this.state))
+    }
+  }
+
+  // Fills the matrix with random swatch[] indexes
+  randomizeMatrix() {
+    for (let x = 0; x < this.matrix.length; x++) {
+      for (let y = 0; y < this.matrix[0].length; y++) {
+        const max = this.swatch.length
+        const swatchIndex = Math.floor(Math.random() * max)
+        this.matrix[x][y] = swatchIndex
+      }
+    }
+    localStorage.setItem('matrix', JSON.stringify(this.matrix))
+  }
+
+  swapColor(x, y) {
+    let index = this.matrix[x][y]
+    this.matrix[x][y] = (index < this.swatch.length) ? index++ : 0
+    localStorage.setItem('matrix', JSON.stringify(this.matrix))
+  }
+
+  setState(key, val) {
+    this.state[key] = val
+    localStorage.setItem('state', JSON.stringify(this.state))
+  }
+
+  getState(key) {
+    return this.state[key]
+  }
+}
 
 export const store = new Store()
